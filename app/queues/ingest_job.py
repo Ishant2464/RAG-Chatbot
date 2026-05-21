655731c9418e9e7bfbcd07d289a55147a2af5da8
Ishant2464 <ishant2464@gmail.com>
2026-05-21 15:06:10 +0530

    Updated ingest job

--- app/queues/ingest_job.py
+++ app/queues/ingest_job.py
@@ -1,12 +1,11 @@
 from redis import Redis
 from app.core.config import settings
 from rq import Queue
-from app.queues.worker_tasks import process_doc
+from app.services.ingest_service import process_document 
 
 redis_conn = Redis.from_url(settings.REDIS_URL)
 q = Queue(connection=redis_conn, default_timeout=settings.RQ_JOB_TIMEOUT)
 
-
-def enqueue_ingest(file_path: str) -> str:
-    job = q.enqueue(process_doc, file_path, job_timeout=None)
+def enqueue_ingest(storage_url: str) -> str:
+    job = q.enqueue(process_document, storage_url, job_timeout=None)
     return job.id
