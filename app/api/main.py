8b5a3e9cbac409abe781ade4420af822702bb927
Ishant2464 <ishant2464@gmail.com>
2026-05-21 17:00:39 +0530

    Updated main.py

--- app/api/main.py
+++ app/api/main.py
@@ -13,6 +13,9 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "https://*.vercel.app",
+        "rag-chatbot-sage.vercel.app",
+        "https://rag-chatbot-git-main-ishant2464s-projects.vercel.app/",
+        "rag-chatbot-bt19wswaf-ishant2464s-projects.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
